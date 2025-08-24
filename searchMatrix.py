ccd1366317acfc6e17b890fcd0f084054392127d
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-24 14:39:41 -0500

    Update searchMatrix.py

--- searchMatrix.py
+++ searchMatrix.py
@@ -17,3 +17,7 @@ class Solution(object):
         bottom = len(matrix) - 1
         left = 0
         right = len(matrix[0]) - 1
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
