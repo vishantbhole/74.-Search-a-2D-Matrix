79b7189df8ea530d2573ef00fde6de25f3c00c80
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-24 14:39:10 -0500

    Update searchMatrix.py

--- searchMatrix.py
+++ searchMatrix.py
@@ -11,3 +11,9 @@ class Solution(object):
         res = []
         if not matrix:
             return res
+
+
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
